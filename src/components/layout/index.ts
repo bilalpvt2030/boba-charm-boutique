a6a63bc4b601707a473214338106d0f085c6fee9
bilalpvt2030 <bilalahmedpvt2030@gmail.com>
2026-05-30 02:18:43 +0530

    Add layout module exports

---
+++ src/components/layout/index.ts
@@ -0,0 +1,7 @@
+export { Navbar } from './Navbar';
+export { Footer } from './Footer';
+export { Container, Section, Grid } from './Container';
+
+export type { NavbarProps, NavItem } from './Navbar';
+export type { FooterProps } from './Footer';
+export type { ContainerProps, SectionProps, GridProps } from './Container';
