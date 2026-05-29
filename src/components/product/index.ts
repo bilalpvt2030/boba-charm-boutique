46cb978bd023d2e8090f329e94a9b48afbbf8424
bilalpvt2030 <bilalahmedpvt2030@gmail.com>
2026-05-30 04:56:43 +0530

    fix: correct product barrel file path

---
+++ src/components/product/index.ts
@@ -0,0 +1,5 @@
+export { ProductCard } from './ProductCard';
+export { ProductPrice } from './ProductPrice';
+export { ProductImage } from './ProductImage';
+export { ProductRating } from './ProductRating';
+export { ProductBadge } from './ProductBadge';
