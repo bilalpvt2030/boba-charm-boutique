b06d46d27c15fd4c2224371d943e743eff210874
bilalpvt2030 <bilalahmedpvt2030@gmail.com>
2026-05-30 02:15:17 +0530

    Update UI exports to include Typography components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,3 @@
+export { Button } from './Button';
+export { Heading, Text, Label, Description } from './Typography';
+export type { HeadingProps, TextProps, LabelProps, DescriptionProps } from './Typography';
